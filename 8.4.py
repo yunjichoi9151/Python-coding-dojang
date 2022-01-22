5cdf824256feddfac5cac5e2210e0c15f320db3a
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-22 22:32:57 +0900

    Add 8 chpater file

---
+++ 8.4.py
@@ -0,0 +1,9 @@
+# 파이썬 코딩 도장
+# Chapter 8 연습문제
+
+korean = 92
+english = 47
+mathematics = 86
+science = 81
+
+print(korean >= 50 and english >= 50 and mathematics >= 50 and science >= 50)
---
+++ 8.5.py
@@ -0,0 +1,6 @@
+# 파이썬 코딩 도장
+# Chapter 8 심사문제
+
+korean, english, mathematics, science = map(int, input().split())
+
+print(korean >= 90 and english > 80 and mathematics > 85 and science >= 80)
