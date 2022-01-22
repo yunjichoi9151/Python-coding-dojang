088b9cb85b1ea89aff77d9cb9a172e6c0cfb2c32
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-22 22:12:44 +0900

    Add 7 chpater file

---
+++ 7.4.py
@@ -0,0 +1,12 @@
+# 파이썬 코딩 도장
+# Chapter 7 연습문제
+
+year = 2000
+month = 10
+day = 27
+hour = 11
+minute = 43
+second = 59
+
+print(year, month, day, sep='/', end=' ')
+print(hour, minute, second, sep=':')
---
+++ 7.5.py
@@ -0,0 +1,7 @@
+# 파이썬 코딩 도장
+# Chapter 7 심사문제
+
+year, month, day, hour, minute, second = input().split()
+
+print(year, month, day, sep='-', end='T')
+print(hour, minute, second, sep=':')
