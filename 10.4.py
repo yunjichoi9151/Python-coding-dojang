fccfc9a39ebde15cb3a01b7be68381d9164e2054
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-24 23:07:32 +0900

    Add 10 chapter file

---
+++ 10.4.py
@@ -0,0 +1,6 @@
+# 파이썬 코딩 도장
+# Chapter 10 연습문제
+
+a = list(range(5, -10, -2))
+
+print(a)
---
+++ 10.5.py
@@ -0,0 +1,7 @@
+# 파이썬 코딩 도장
+# Chapter 10 심사문제
+
+a = int(input())
+b = tuple(range(-10, 10, a))
+
+print(b)
