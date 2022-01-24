3c75b37a68b02326e57673878fbf9dea64175912
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-24 23:03:35 +0900

    시작 주석 수정

--- 9.3.py
+++ 9.3.py
@@ -1,3 +1,6 @@
+# 파이썬 코딩 도장
+# Chapter 9 연습문제
+
 s = '''Python is a programming language that lets you work quickly
 and
 integrate systems more effectively.'''
--- 9.4.py
+++ 9.4.py
@@ -1,3 +1,6 @@
+# 파이썬 코딩 도장
+# Chapter 9 심사문제
+
 s = ''''Python' is a "programming language"
 that lets you work quickly
 and
