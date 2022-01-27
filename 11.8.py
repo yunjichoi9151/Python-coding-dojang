c1ea8f67458e126c21347623b6888f660d57ed23
yunjichoi9151 <cyjcode99@gmail.com>
2022-01-27 21:43:01 +0900

    Add 11 chapter file

---
+++ 11.8.py
@@ -0,0 +1,7 @@
+# 파이썬 코딩 도장
+# Chapter 11 심사문제
+
+x = input().split()
+
+del x[-5:]
+print(tuple(x))
---
+++ 11.9.py
@@ -0,0 +1,7 @@
+# 파이썬 코딩 도장
+# Chapter 11 심사문제
+
+a = input()
+b = input()
+
+print(a[1::2]+b[0::2])
