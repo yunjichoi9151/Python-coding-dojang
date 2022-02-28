bc362ba935880cc3466ef8f6b4ba9de1b6de0987
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-01 00:23:11 +0900

    Add 34 chapter file

---
+++ 34.5.py
@@ -0,0 +1,15 @@
+# 파이썬 코딩 도장
+# Chapter 34 연습문제
+
+class Knight:
+    def __init__(self, health, mana, armor):
+        self.health = health
+        self.mana = mana
+        self.armor = armor
+ 
+    def slash(self):
+        print('베기')
+        
+x = Knight(health=542.4, mana=210.3, armor=38)
+print(x.health, x.mana, x.armor)
+x.slash()
---
+++ 34.6.py
@@ -0,0 +1,16 @@
+# 파이썬 코딩 도장
+# Chapter 34 심사문제
+
+class Annie:
+    def __init__(self, health, mana, ability_power):
+        self.health = health
+        self.mana = mana
+        self.ability_power = ability_power
+ 
+    def tibbers(self):
+        print('티버: 피해량', self.ability_power * 0.65 + 400)
+
+health, mana, ability_power = map(float, input().split())
+ 
+x = Annie(health=health, mana=mana, ability_power=ability_power)
+x.tibbers()
