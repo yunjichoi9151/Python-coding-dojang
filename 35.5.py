8dcdb7786ddb21bf16a78488eeb85f3c6f545ec3
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-02 01:25:52 +0900

    Add 35 chapter file

---
+++ 35.5.py
@@ -0,0 +1,13 @@
+# 파이썬 코딩 도장
+# Chapter 35 연습문제
+
+class Date:
+    @staticmethod
+    def is_date_valid(date_string):
+        year, month, day = map(int, date_string.split('-'))
+        return month <= 12 and day <= 31
+ 
+if Date.is_date_valid('2000-10-31'):
+    print('올바른 날짜 형식입니다.')
+else:
+    print('잘못된 날짜 형식입니다.')
