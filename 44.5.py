4cb9f2ae419c7f34e328ac0632a3cf4a10683930
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-04 11:19:05 +0900

    Add 44 chapter file

---
+++ 44.5.py
@@ -0,0 +1,8 @@
+# 파이썬 코딩 도장
+# Chapter 44 연습문제
+
+from math import ceil, floor
+
+x = 1.5
+ 
+print(ceil(x), floor(x))
---
+++ 44.6.py
@@ -0,0 +1,6 @@
+# 파이썬 코딩 도장
+# Chapter 44 심사문제
+
+import math
+r = float(input())
+print(r * r * math.pi)
